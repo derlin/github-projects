915f0eb48c4de94b4687285c0a6fd8a6d9177354
Lucy Linder <lucy.derlin@gmail.com>
2023-01-05 17:16:06 +0100

    feat: loading, dev icons

--- script.js
+++ script.js
@@ -4,17 +4,18 @@ const repoList = document.querySelector('.repo-list');
 const reposSection = document.querySelector('.repos');
 const filterInput = document.querySelector('.filter-repos');
 
+const headers = {};
+// const headers = {
+//     headers: {
+//         Accept: 'application/vnd.github+json',
+//         Authorization: '<TOKEN>'
+//     }
+// };
 
 // get information from github profile
 const getProfile = async function () {
     const res = await fetch(
-        `https://api.github.com/users/${username}`
-        // {
-        //     headers: {
-        //         Accept: 'application/vnd.github+json',
-        //         Authorization: 'token your-personal-access-token-here'
-        //     }
-        // }
+        `https://api.github.com/users/${username}`, headers
     );
     const profile = await res.json();
     displayProfile(profile);
@@ -30,13 +31,10 @@ const displayProfile = function (profile) {
     </figure>
     <div>
         <h2><a href=${profile.blog}><strong>${profile.name}</strong></a></h2>
+        <strong>@${profile.login} </strong>
         <p>${profile.bio}</p>
         <p>
-            <strong>Location:</strong> ${profile.location}
-        </p>
-        <p>
-            <strong>@${profile.login} </strong>
-            Repos: ${profile.public_repos}
+            Repos: ${profile.public_repos} /
             Gists: ${profile.public_gists}
         </p>
     </div>
@@ -49,18 +47,13 @@ const getRepos = async function () {
     let res;
     for (let i = 1; i <= maxPages; i++) {
         res = await fetch(
-            `https://api.github.com/users/${username}/repos?&sort=pushed&per_page=100&page=${i}`
-            // {
-            //     headers: {
-            //         Accept: 'application/vnd.github+json',
-            //         Authorization:
-            //             'token your-personal-access-token-here'
-            //     }
-            // }
+            `https://api.github.com/users/${username}/repos?&sort=pushed&per_page=100&page=${i}`,
+            headers
         );
         let data = await res.json();
         repos = repos.concat(data);
     }
+    document.getElementById("loading").remove();
     displayRepos(repos);
 };
 getRepos();
@@ -69,13 +62,16 @@ getRepos();
 const displayRepos = function (repos) {
     filterInput.classList.remove('hide');
     for (const repo of repos) {
+        if (repo.fork || !repo.description) continue;
         let listItem = document.createElement('li');
+        if (repo.homepage) repo.homepage = `<a class="homepage" href="${repo.homepage}" target="_blank">homepage</a>`;
         listItem.classList.add('repo');
         listItem.innerHTML = `
             <h3>${repo.name}</h3>
             <p>${repo.description}</p> 
-            <span>${devicons[repo.language]}</span>
-            <a href=${repo.html_url}>View Project</a>`;
+            <span>${devicons[repo.language] || ""} ${repo.language || ""}</span>
+            ${repo.homepage ?? ""}
+            <a class="view" href=${repo.html_url} target="_blank">View Project</a>`;
         repoList.append(listItem);
     }
 };
@@ -98,55 +94,57 @@ filterInput.addEventListener('input', function (e) {
 
 // for programming language icons
 const devicons = {
-    Assembly: '<i class="devicon-labview-plain colored"></i> Assembly',
-    'C#': '<i class="devicon-csharp-plain colored"></i> C#',
-    'C++': '<i class="devicon-cplusplus-plain colored"></i> C++',
-    C: '<i class="devicon-c-plain colored"></i> C',
-    Clojure: '<i class="devicon-clojure-plain colored"></i> C',
-    CoffeeScript: '<i class="devicon-coffeescript-plain colored"></i> CoffeeScript',
-    Crystal: '<i class="devicon-crystal-plain colored"></i> Crystal',
-    CSS: '<i class="devicon-css3-plain colored"></i> CSS',
-    Dart: '<i class="devicon-dart-plain colored"></i> Dart',
-    Dockerfile: '<i class="devicon-docker-plain colored"></i> Docker',
-    Elixir: '<i class="devicon-elixir-plain colored"></i> Elixir',
-    Elm: '<i class="devicon-elm-plain colored"></i> Elm',
-    Erlang: '<i class="devicon-erlang-plain colored"></i> Erlang',
-    'F#': '<i class="devicon-fsharp-plain colored"></i> F#',
-    Go: '<i class="devicon-go-plain colored"></i> Go',
-    Groovy: '<i class="devicon-groovy-plain colored"></i> Groovy',
-    HTML: '<i class="devicon-html5-plain colored"></i> HTML',
-    Haskell: '<i class="devicon-haskell-plain colored"></i> Haskell',
-    Java: '<i class="devicon-java-plain colored" style="color: #ffca2c"></i> Java',
-    JavaScript: '<i class="devicon-javascript-plain colored"></i> JavaScript',
-    Julia: '<i class="devicon-julia-plain colored"></i> Julia',
-    'Jupyter Notebook': '<i class="devicon-jupyter-plain colored"></i> Jupyter',
-    Kotlin: '<i class="devicon-kotlin-plain colored" style="color: #796bdc"></i> Kotlin',
-    Latex: '<i class="devicon-latex-plain colored"></i> Latex',
-    Lua: '<i class="devicon-lua-plain-wordmark colored" style="color: #0000d0"></i> Lua',
-    Matlab: '<i class="devicon-matlab-plain colored"></i> Matlab',
-    Nim: '<i class="devicon-nixos-plain colored" style="color: #FFC200"></i> Nim',
-    Nix: '<i class="devicon-nixos-plain colored"></i> Nix',
-    ObjectiveC: '<i class="devicon-objectivec-plain colored"></i> ObjectiveC',
-    OCaml: '<i class="devicon-ocaml-plain colored"></i> OCaml',
-    Perl: '<i class="devicon-perl-plain colored"></i> Perl',
-    PHP: '<i class="devicon-php-plain colored"></i> PHP',
-    PLSQL: '<i class="devicon-sqlite-plain colored"></i> PLSQL',
+    Assembly: '<i class="devicon-labview-plain colored"></i>',
+    'C#': '<i class="devicon-csharp-plain colored"></i>',
+    'C++': '<i class="devicon-cplusplus-plain colored"></i>',
+    C: '<i class="devicon-c-plain colored"></i>',
+    Clojure: '<i class="devicon-clojure-plain colored"></i>',
+    CoffeeScript: '<i class="devicon-coffeescript-plain colored"></i>',
+    Crystal: '<i class="devicon-crystal-plain colored"></i>',
+    CSS: '<i class="devicon-css3-plain colored"></i>',
+    Dart: '<i class="devicon-dart-plain colored"></i>',
+    Dockerfile: '<i class="devicon-docker-plain colored"></i>',
+    Elixir: '<i class="devicon-elixir-plain colored"></i>',
+    Elm: '<i class="devicon-elm-plain colored"></i>',
+    Erlang: '<i class="devicon-erlang-plain colored"></i>',
+    'F#': '<i class="devicon-fsharp-plain colored"></i>',
+    Go: '<i class="devicon-go-plain colored"></i>',
+    Groovy: '<i class="devicon-groovy-plain colored"></i>',
+    HTML: '<i class="devicon-html5-plain colored"></i>',
+    Haskell: '<i class="devicon-haskell-plain colored"></i>',
+    Java: '<i class="devicon-java-plain colored" style="color: #ffca2c"></i>',
+    JavaScript: '<i class="devicon-javascript-plain colored"></i>',
+    Julia: '<i class="devicon-julia-plain colored"></i>',
+    'Jupyter Notebook': '<i class="devicon-jupyter-plain colored"></i>',
+    Kotlin: '<i class="devicon-kotlin-plain colored" style="color: #796bdc"></i>',
+    Latex: '<i class="devicon-latex-plain colored"></i>',
+    Lua: '<i class="devicon-lua-plain-wordmark colored" style="color: #0000d0"></i>',
+    Matlab: '<i class="devicon-matlab-plain colored"></i>',
+    Nim: '<i class="devicon-nixos-plain colored" style="color: #FFC200"></i>',
+    Nix: '<i class="devicon-nixos-plain colored"></i>',
+    ObjectiveC: '<i class="devicon-objectivec-plain colored"></i>',
+    OCaml: '<i class="devicon-ocaml-plain colored"></i>',
+    Perl: '<i class="devicon-perl-plain colored"></i>',
+    PHP: '<i class="devicon-php-plain colored"></i>',
+    PLSQL: '<i class="devicon-sqlite-plain colored"></i>',
     Processing:
-        '<i class="devicon-processing-plain colored" style="color: #0096D8"></i> Processing',
-    Python: '<i class="devicon-python-plain colored" style="color: #3472a6"></i> Python',
-    R: '<i class="devicon-r-plain colored"></i> R',
-    Ruby: '<i class="devicon-ruby-plain colored"></i> Ruby',
-    Rust: '<i class="devicon-rust-plain colored" style="color: #DEA584"></i> Rust',
-    Sass: '<i class="devicon-sass-original colored"></i> Sass',
-    Scala: '<i class="devicon-scala-plain colored"></i> Scala',
-    Shell: '<i class="devicon-bash-plain colored" style="color: #89E051"></i> Shell',
-    Solidity: '<i class="devicon-solidity-plain colored"></i> Solidity',
-    Stylus: '<i class="devicon-stylus-plain colored"></i> Stylus',
-    Svelte: '<i class="devicon-svelte-plain colored"></i> Svelte',
-    Swift: '<i class="devicon-swift-plain colored"></i> Swift',
-    Terraform: '<i class="devicon-terraform-plain colored"></i> Terraform',
-    TypeScript: '<i class="devicon-typescript-plain colored"></i> TypeScript',
-    'Vim Script': '<i class="devicon-vim-plain colored"></i> Vim Script',
-    Vue: '<i class="devicon-vuejs-plain colored"></i> Vue',
-    null: '<i class="devicon-markdown-original"></i> Markdown'
+        '<i class="devicon-processing-plain colored" style="color: #0096D8"></i>',
+    Python: '<i class="devicon-python-plain colored" style="color: #3472a6"></i>',
+    R: '<i class="devicon-r-plain colored"></i>',
+    Ruby: '<i class="devicon-ruby-plain colored"></i>',
+    Rust: '<i class="devicon-rust-plain colored" style="color: #DEA584"></i>',
+    Sass: '<i class="devicon-sass-original colored"></i>',
+    Scala: '<i class="devicon-scala-plain colored"></i>',
+    Shell: '<i class="devicon-bash-plain colored" style="color: #89E051"></i>',
+    Solidity: '<i class="devicon-solidity-plain colored"></i>',
+    Stylus: '<i class="devicon-stylus-plain colored"></i>',
+    Svelte: '<i class="devicon-svelte-plain colored"></i>',
+    Swift: '<i class="devicon-swift-plain colored"></i>',
+    Terraform: '<i class="devicon-terraform-plain colored"></i>',
+    TeX: '<i class="devicon-latex-plain colored"></i>',
+    TypeScript: '<i class="devicon-typescript-plain colored"></i>',
+    'Vim Script': '<i class="devicon-vim-plain colored"></i>',
+    VimL: '<i class="devicon-vim-plain colored"></i>',
+    Vue: '<i class="devicon-vuejs-plain colored"></i>',
+    null: '<i class="devicon-markdown-original"></i>'
 };
